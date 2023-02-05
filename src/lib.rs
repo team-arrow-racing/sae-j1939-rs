a3fb7a0ff7ad87ed25d9af4e49e2199f06913fa6
Liam Kinne <liam@kinne.io>
2023-02-05 12:15:16 +1000

    add creating identifiers from raw bits

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,19 @@ pub struct IdStandard {
 }
 
 impl IdStandard {
+    /// Create standard identifier from raw bits.
+    pub fn new(raw: u16) -> Self {
+        let priority = (raw >> 8) as u8;
+        assert!(priority < 8);
+        let source_address = raw as u8;
+
+        IdStandard {
+            priority,
+            source_address,
+        }
+    }
+
+    /// Convert to raw bits.
     pub fn to_bits(&self) -> u16 {
         assert!(self.priority < 8);
 
@@ -28,6 +41,26 @@ pub struct IdExtended {
 }
 
 impl IdExtended {
+    /// Create extended identifier from raw bits.
+    pub fn new(raw: u32) -> Self {
+        let priority = (raw >> 26) as u8;
+        assert!(priority < 8);
+        let ext_data_page = ((raw >> 25) & 0b1) != 0;
+        let data_page = ((raw >> 24) & 0b1) != 0;
+        let pdu_format = (raw >> 16) as u8;
+        let pdu_specific = (raw >> 8) as u8;
+        let source_address = raw as u8;
+
+        IdExtended {
+            priority,
+            ext_data_page,
+            data_page,
+            pdu_format,
+            pdu_specific,
+            source_address,
+        }
+    }
+
     /// Convert to raw bits.
     pub fn to_bits(&self) -> u32 {
         assert!(self.priority < 8);
@@ -48,6 +81,14 @@ mod tests {
     use super::*;
 
     #[test]
+    fn bits_to_standard_id() {
+        let id = IdStandard::new(0x6FE);
+
+        assert_eq!(id.priority, 6);
+        assert_eq!(id.source_address, 0xFE);
+    }
+    
+    #[test]
     fn id_standard_to_bits() {
         // Example PGN 0xF333: High Voltage Energy Storage Pack 26 Data 1
         let id = IdStandard {
@@ -55,6 +96,18 @@ mod tests {
             source_address: 0xFE,
         };
         assert_eq!(id.to_bits(), 0x6FE);
+    }
+
+    #[test]
+    fn bits_to_extended_id() {
+        let id = IdExtended::new(0x0CF004FE);
+
+        assert_eq!(id.priority, 3);
+        assert_eq!(id.ext_data_page, false);
+        assert_eq!(id.data_page, false);
+        assert_eq!(id.pdu_format, 0xF0);
+        assert_eq!(id.pdu_specific, 0x04);
+        assert_eq!(id.source_address, 0xFE);
     }
 
     #[test]
