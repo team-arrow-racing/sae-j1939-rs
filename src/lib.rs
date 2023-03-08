2fa664da7154bfa97fa3f52446bb1bd05e5e3c90
Liam Kinne <liam@kinne.io>
2023-03-09 00:01:57 +1000

    add parameter value and control value types

--- src/lib.rs
+++ src/lib.rs
@@ -142,3 +142,26 @@ mod tests {
         assert_eq!(id.to_bits(), 0x18F122FE);
     }
 }
+
+/// Transmitted values for discrete parameters (i.e. measured).
+#[derive(Default, Copy, Clone)]
+pub enum ParameterValue {
+    Disabled = 0b00,
+    Enabled = 0b01,
+    Error = 0b10,
+    #[default]
+    NotAvailable = 0b11,
+}
+
+/// Transmitted values for control commands (i.e. status).
+#[derive(Default, Copy, Clone)]
+pub enum ControlValue {
+    /// Command to disable function (turn off).
+    Disable = 0b00,
+    /// Command to enable function (turn on).
+    Enable = 0b01,
+    // Reserved = 0b10,
+    /// Take no action (leave as is).
+    #[default]
+    NoAction = 0b11,
+}
